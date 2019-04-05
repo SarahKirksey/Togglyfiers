4ccb718d60be11a055c44b073270f235abdf8567
Cibernet <cyberneticGuardian@gmail.com>
2019-04-05 17:41:50 -0500

    updated from master

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Togglyfiers
+Togglyfiers is a Minecraft mod overseen by Sensei Sarah and created by her squad of students.
+
+As for what it is... well, hopefully, people will be able to find out as the mod develops!
---
+++ src/main/java/com/example/exampleaddon/items/ModItems.java
@@ -0,0 +1,34 @@
+package com.example.exampleaddon.items;
+
+import com.mraof.minestuck.item.MinestuckItems;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+import static com.example.exampleaddon.blocks.ModBlocks.*;
+
+public class ModItems {
+	
+	public static Item exampleItem = new Item().setRegistryName("example_item").setUnlocalizedName("exampleItem").setCreativeTab(MinestuckItems.tabMinestuck);
+
+	@SubscribeEvent
+	public static void registerItems(RegistryEvent.Register<Item> event)
+	{
+		IForgeRegistry<Item> registry = event.getRegistry();
+		
+		//ItemBlocks
+		registerItemBlock(registry, new ItemBlock(exampleBlock));
+		
+		registry.register(exampleItem);
+		
+	}
+	
+	private static Item registerItemBlock(IForgeRegistry<Item> registry, ItemBlock item)
+	{
+		registry.register(item.setRegistryName(item.getBlock().getRegistryName()));
+		return item;
+	}
+
+}
